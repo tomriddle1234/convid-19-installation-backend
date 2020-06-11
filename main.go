aa89b900c9efaf59d9de1515566269a0f2fac201
tomriddle_1234 <tomriddle_1234@hotmail.com>
2020-06-11 22:34:50 +0800

    Change port

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	gin.DefaultWriter = io.MultiWriter(f)
 	log.SetOutput(gin.DefaultWriter)
 
-	GetSimpleProdEngine().Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	GetSimpleProdEngine().Run(":8082") // listen and serve on 0.0.0.0:8082 (for windows "localhost:8080")
 }
 
 
